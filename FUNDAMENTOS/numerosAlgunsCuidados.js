f6d4836c7e945467d431ed9bd4919e5d4c931184
nikorodrigues <wnicolasrodrigues1@gmail.com>
2024-09-19 17:30:49 -0300

    objeto Math

--- FUNDAMENTOS/numerosAlgunsCuidados.js
+++ FUNDAMENTOS/numerosAlgunsCuidados.js
@@ -2,6 +2,8 @@ console.log(7/0.000001) //Infinity
 
 //SEMPRE CONVERTER STRING PARA NUMBER, QUANDO A STRING FOR UM NUMBER
 console.log("10.2"/2) //5
+console.log('3' + 2) //32 pois esta concatenando 3 com 2 //A string ganha!! //String tem preferência
+console.log('3' - 2) //1 pois esta subtraindo 2 de 3 (- nao faz sentido em string)
 console.log("Show!" * 2) //NaN
 console.log(0.1 + 0.7) //0.7999999999999999
 //ecma-international.org
---
+++ FUNDAMENTOS/string.js
@@ -0,0 +1,17 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4)) // Retorna o caractere no índice 4
+console.log(escola.charAt(5)) // Retorna o caractere no índice 5
+console.log(escola.charCodeAt(3)) // Retorna o valor do caractere no índice 3 na tabela ASCII
+console.log(escola.indexOf('3')) // Retorna o índice do caractere '3'
+
+console.log(escola.substring(1)) // Retorna a string a partir do índice 1
+console.log(escola.substring(0, 3)) // Retorna a string do índice 0(inicial) até o índice 3(final)
+
+console.log('Escola'.concat(escola).concat("!")) // Concatenação de strings
+console.log('Escola' + escola + "!") // Concatenação de strings
+console.log(escola.replace(3, 'e')) // Substitui o 3 por e
+console.log(escola.replace(/\d/g, 'e')) // Substitui todos os dígitos por e
+console.log(escola.replace(/\w/g, 'e')) // Substitui todas as letras por e
+
+console.log('Ana,Maria,Pedro'.split(',')) // Separa a string em um array, utilizando a vírgula como separador
