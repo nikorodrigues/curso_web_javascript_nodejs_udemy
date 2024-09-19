33e31621be5629218afc326112079ea97d8989db
nikorodrigues <wnicolasrodrigues1@gmail.com>
2024-09-19 15:29:26 -0300

    tipagem fraca: JS PYTHON ETC...

---
+++ FUNDAMENTOS/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = "Legal"
+console.log(qualquer)
+console.log(typeof qualquer) //typeof é usado para saber o tipo da variável
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nome genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso HAHAHA
+
