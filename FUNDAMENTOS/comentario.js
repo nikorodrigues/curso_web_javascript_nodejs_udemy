be70fd0f9f023567d7c055e1eda7c1a377c55e72
nikorodrigues <wnicolasrodrigues1@gmail.com>
2024-09-19 14:53:17 -0300

    organizacao de codigo e comentarios

---
+++ FUNDAMENTOS/comentario.js
@@ -0,0 +1,26 @@
+// comentario de uma linha
+console.log("linha1")
+
+/*
+comentario de
+multiplas linhas
+ */
+
+console.log("linha2")
+/*
+ * comentario de
+ * multiplas linhas
+ *
+ *
+ */
+
+/*
+* prezando pelo codigo:
+* codigo claro, limpo e objetivo
+* comentarios que expliquem detalhes sobre o codigo ou funcao
+* ex: este codigo e um pouco lento pois interage com 8 servicos diferentes,
+* caso tenha problemas com X funcao, verificar Y servico
+* 1-evite comentarios idiotas
+* 2-procure nomes claros, limpos
+* 3-comente tudo que acha que tem necessidade
+ */
---
+++ FUNDAMENTOS/organizacao.js
@@ -0,0 +1,14 @@
+
+// Como o javascript e organizado?
+// 1- sentencas de codigos
+// 2- blocos de codigos(agrupamento de sentencas)
+
+console.log("linha de codigo:")
+
+// Um bloco sempre esta dentro do outro
+{
+    {
+     console.log("Ola");
+     console.log('Mundo!')
+    }
+}
